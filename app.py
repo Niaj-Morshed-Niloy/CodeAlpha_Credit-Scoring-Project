6c47ee0288464809b316d18d2bba7e2bd091b57b
Md. Niaj Morshed <niaj9064@gmail.com>
2026-04-10 21:21:19 +0600

    Initial commit: Add credit scoring project files

---
+++ app.py
@@ -0,0 +1,61 @@
+import gradio as gr
+import pandas as pd
+import pickle
+
+with open("CreditScore.pkl", "rb") as f:
+    model = pickle.load(f)
+
+def predict_credit(
+    revolving_utilization,
+    age,
+    past_due_30_59,
+    debt_ratio,
+    monthly_income,
+    open_credit_lines,
+    past_due_90,
+    real_estate_loans,
+    past_due_60_89,
+    dependents
+):
+
+    input_data = pd.DataFrame([{
+        "RevolvingUtilizationOfUnsecuredLines": revolving_utilization,
+        "age": age,
+        "NumberOfTime30-59DaysPastDueNotWorse": past_due_30_59,
+        "DebtRatio": debt_ratio,
+        "MonthlyIncome": monthly_income,
+        "NumberOfOpenCreditLinesAndLoans": open_credit_lines,
+        "NumberOfTimes90DaysLate": past_due_90,
+        "NumberRealEstateLoansOrLines": real_estate_loans,
+        "NumberOfTime60-89DaysPastDueNotWorse": past_due_60_89,
+        "NumberOfDependents": dependents
+    }])
+
+    prob = model.predict_proba(input_data)[0][1]
+    threshold = 0.42
+
+    if prob >= threshold:
+        return f"High Risk of Default (Risk Score: {prob:.2f})"
+    else:
+        return f"Low Risk of Default (Risk Score: {prob:.2f})"
+
+interface = gr.Interface(
+    fn=predict_credit,
+    inputs=[
+        gr.Number(label="Revolving Utilization"),
+        gr.Number(label="Age"),
+        gr.Number(label="30-59 Days Past Due"),
+        gr.Number(label="Debt Ratio"),
+        gr.Number(label="Monthly Income"),
+        gr.Number(label="Open Credit Lines"),
+        gr.Number(label="90+ Days Past Due"),
+        gr.Number(label="Real Estate Loans"),
+        gr.Number(label="60-89 Days Past Due"),
+        gr.Number(label="Number of Dependents")
+    ],
+    outputs="text",
+    title="Credit Risk Prediction System",
+    description="Predicts whether a person is high or low risk for loan default using financial history data."
+)
+
+interface.launch()
---
+++ train_file.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+# from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+df=pd.read_csv("cs.csv")
+df = df.drop(columns=["Unnamed: 0"])
+
+imputer = SimpleImputer(strategy="median")
+df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+X = df_imputed.drop("SeriousDlqin2yrs", axis=1)
+y = df_imputed["SeriousDlqin2yrs"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# model = LogisticRegression(max_iter=1000, class_weight='balanced')
+# y_pred = model.predict(X_test)
+
+model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=10,
+    random_state=42,
+    class_weight='balanced'
+)
+model.fit(X_train, y_train)
+
+y_prob = model.predict_proba(X_test)[:, 1]
+y_pred = (y_prob > 0.42).astype(int)
+
+# y_pred = model.predict(X_test)
+# y_prob = model.predict_proba(X_test)[:, 1]
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_prob)
+
+print(f"1. Accuracy  : {accuracy:.4f}")
+print(f"2. Precision : {precision:.4f}")
+print(f"3. Recall    : {recall:.4f}")
+print(f"4. F1-Score  : {f1:.4f}")
+print(f"5. ROC-AUC   : {roc_auc:.4f}")
+
+with open("CreditScore.pkl", "wb") as f:
+    pickle.dump(model, f)
